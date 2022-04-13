118e33e1ea421590ceedd1c0cec843ca9d63d034
jwarlima <zaz097@hotmail.com>
2022-04-13 12:22:04 -0300

    Layout responsivo + Forms

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -12,73 +12,9 @@ import { EventoService } from '../../services/evento.service';
   // providers: [EventoService]
 })
 export class EventosComponent implements OnInit {
-  modalRef?: BsModalRef;
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
-  public larguraImagem: number = 150;
-  public margemImagem: number = 2;
-  public exibirImagem: boolean = true;
-  private _filtroLista: string = '';
-
-  public get filtroLista(): string{
-    return this._filtroLista;
-  }
-
-  public set filtroLista(value: string){
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-
-  public filtrarEventos(filtrarPor:string):Evento[]{
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: { tema: string; local: string}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    );
-  }
-
-
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-    ) { }
-
-  openModal(template: TemplateRef<any>):void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('Evento excluído com sucesso.', 'Exclusão de evento')
-  }
-
-  decline(): void {
-    this.modalRef?.hide();
-  }
 
   public ngOnInit(): void {
-    this.spinner.show();
-    this.getEventos();
-  }
 
-  public alterarImagem(): void{
-    this.exibirImagem = !this.exibirImagem;
-  }
-
-  public getEventos():void{
-    this.eventoService.getEventos().subscribe({
-      next: (_eventos: Evento[]) => {
-        this.eventos = _eventos;
-        this.eventosFiltrados = this.eventos;
-      },
-      error: (error:any) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar eventos.', 'Erro')
-      },
-      complete: () => this.spinner.hide()
-    });
   }
 }
 
